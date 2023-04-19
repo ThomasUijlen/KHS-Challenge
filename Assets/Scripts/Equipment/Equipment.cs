055ed3213cbf97b50fb2c7ff7c5f91f50d37ca8a
ThomasUijlen <thomasuijlen@hotmail.com>
2023-04-19 20:39:39 +0200

    Added SMG model prefab

---
+++ Assets/Scripts/Equipment/Equipment.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+[RequireComponent(typeof(XRGrabInteractable))]
+public class Equipment : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
