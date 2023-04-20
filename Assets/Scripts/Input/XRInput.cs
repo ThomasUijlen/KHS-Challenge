2d6b64eedf27ca78a1c072e7afa1235f5f1e0080
ThomasUijlen <thomasuijlen@hotmail.com>
2023-04-20 12:54:58 +0200

    Input improvements

--- Assets/Scripts/Input/XRInput.cs
+++ Assets/Scripts/Input/XRInput.cs
@@ -13,14 +13,50 @@ public class XRInput : MonoBehaviour
     private InputActionProperty secondary;
 
     public bool TriggerPressed() {
-        return trigger.action.IsPressed();
+        return IsPressed(trigger.action);
+    }
+
+    public bool TriggerJustPressed() {
+        return JustPressed(trigger.action);
+    }
+
+    public bool TriggerJustReleased() {
+        return JustReleased(trigger.action);
     }
 
     public bool PrimaryPressed() {
         return primary.action.IsPressed();
     }
 
+    public bool PrimaryJustPressed() {
+        return JustPressed(primary.action);
+    }
+
+    public bool PrimaryJustReleased() {
+        return JustReleased(primary.action);
+    }
+
     public bool SecondaryPressed() {
         return secondary.action.IsPressed();
+    }
+
+    public bool SecondaryJustPressed() {
+        return JustPressed(secondary.action);
+    }
+
+    public bool SecondaryJustReleased() {
+        return JustReleased(secondary.action);
+    }
+
+    private bool IsPressed(InputAction action) {
+        return action.IsPressed();
+    }
+
+    private bool JustPressed(InputAction action) {
+        return action.triggered && action.ReadValue<float>() > 0;
+    }
+
+    private bool JustReleased(InputAction action) {
+        return action.triggered && action.ReadValue<float>() == default;
     }
 }
